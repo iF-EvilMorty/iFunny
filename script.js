3f8312169cd91f223de57977cfcd8a5f82864f21
iF-EvilMorty <80785677+iF-EvilMorty@users.noreply.github.com>
2021-03-19 06:34:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 
-window.onscroll = function() {myFunction()};
-var header = document.getElementById("fixedheader");
-var sticky = header.offsetTop;
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {myHeader
-    header.classList.remove("sticky");
-  }
-}
