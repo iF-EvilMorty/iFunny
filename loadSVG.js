f463cb9cfc86bc90e940d5497afd9db151338100
iF-EvilMorty <80785677+iF-EvilMorty@users.noreply.github.com>
2021-03-21 06:20:36 -0400

    Create loadSVG.js

---
+++ loadSVG.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var SVG_logo_icon ="";
+  var SVG_logo_text ="";
+  var SVG_featured_icon ="<use xlink:href="#icon_star-featured"><symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" id="icon_star-featured"><g fill="none" fill-rule="evenodd"><circle cx="10" cy="10" r="10" fill="#FC0"></circle><circle cx="10" cy="10" r="8" fill="#18191E"></circle><path fill="#FC0" d="M11.906 8.017l-1.164-3.54A.798.798 0 0 0 10.007 4a.798.798 0 0 0-.735.476L8.094 8.022H4.8c-.44.014-.799.366-.799.785 0 .3.155.55.387.676l2.687 1.472-1.005 2.96a.776.776 0 0 0-.06.3c0 .433.358.785.8.785a.805.805 0 0 0 .494-.169l2.704-1.858 2.704 1.858a.805.805 0 0 0 .494.169c.442 0 .8-.352.8-.786a.776.776 0 0 0-.06-.3l-1.005-2.959 2.673-1.472A.756.756 0 0 0 16 8.807c0-.42-.358-.771-.8-.785l-3.294-.005z"></path></g></symbol></use>";
+}
+document.getElementById("TheBody").innerHTML = SVG_logo_icon;});
+document.getElementById("TheBody").innerHTML = SVG_logo_text;});
+document.getElementByClass("featured_icon").innerHTML = SVG_featured_icon;});
